f9c27471127a9269167084a0ac62323b57400d60
Julia Petrash <36237100+juliapertash@users.noreply.github.com>
2019-03-16 22:47:07 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,18 @@
+var cvs = document.getElementById("canvas");
+	var ctx = cvs.getContext("2d");
+	
+function Click()
+{
+	var x=100;
+	var y=100;
+	cvs.onclick=function(e)
+	{
+	x = Number(e.clientX);
+	 y = Number(e.clientY);
+	 ctx.beginPath();
+ctx.arc(x, y, 50, 0, 2 * Math.PI);
+ctx.stroke(); 
+			}
+    
+	 
+}
